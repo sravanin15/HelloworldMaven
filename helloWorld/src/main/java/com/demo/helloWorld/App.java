5a90b18dd82151f1a74914ec9b309c2e88d76548
sravanin15 <78396210+sravanin15@users.noreply.github.com>
2021-02-23 10:04:12 +0530

    Update App.java

--- helloWorld/src/main/java/com/demo/helloWorld/App.java
+++ helloWorld/src/main/java/com/demo/helloWorld/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        int sum=add(4,5);
+        int sum=com.demo.helloWorld.Add.add(4,5);
         System.out.println( sum );
     }
 }
